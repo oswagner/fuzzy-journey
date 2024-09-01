5a53b3ddc408134fed78428cdbcca0e042e93958
Wagner Santos <prog.wagner@gmail.com>
2024-09-01 19:55:50 -0300

    feat: add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,35 @@
+import Search from "@/app/ui/search";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
 import { Metadata } from "next";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page || 1);
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
